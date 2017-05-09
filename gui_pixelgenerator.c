ba5f0bde06c960f8644a030573551842820583ab
SeDi343 <sedi343@gmail.com>
2017-05-09 19:13:50 +0200

    moved save picture button into headerbar

--- gui_pixelgenerator.c
+++ gui_pixelgenerator.c
@@ -10,6 +10,7 @@
  *                                 and inputs for offset & zoom of mandelbrot set
  *          Rev.: 04, 09.05.2017 - Changed boxes scales, due to bug with
  *                                 separators
+ *          Rev.: 05, 09.05.2017 - Moved save picture button into headerbar
  *
  * \information
  *
@@ -45,15 +46,13 @@ static void activate (GtkApplication *app, gpointer data)
 	GtkWidget *name_zoom;
 	GtkWidget *name_color;
 	GtkWidget *clr_button, *generate_button;
+	GtkWidget *save_button;
 	GtkWidget *headerbar;
+	GtkWidget *image;
 	GtkWidget *box_1; /* mandelbrot picture */
 	GtkWidget *box_2; /* separator 1 */
 	GtkWidget *box_3; /* buttons */
 	GtkWidget *sep_image;
-/*	GtkWidget *sep_iterations;*/
-/*	GtkWidget *sep_offset_x;*/
-/*	GtkWidget *sep_offset_y;*/
-/*	GtkWidget *sep_zoom;*/
 	GtkStyleContext *context;
 #if GTK_NEW
 	GtkStyleProvider *provider;
@@ -73,7 +72,7 @@ static void activate (GtkApplication *app, gpointer data)
 	window = gtk_application_window_new(app);
 	
 	gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
-	gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720);
+	//gtk_window_set_default_size(GTK_WINDOW(window), 1280, 720);
 	gtk_window_set_default_icon_from_file("icon.jpg", NULL);
 	
 	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
@@ -84,17 +83,20 @@ static void activate (GtkApplication *app, gpointer data)
 	
 	gtk_grid_set_column_homogeneous(GTK_GRID(grid), FALSE);
 	gtk_container_add(GTK_CONTAINER(window), grid);
-	gtk_container_set_border_width(GTK_CONTAINER(window), 0);
+	gtk_container_set_border_width(GTK_CONTAINER(window), 5);
 	
 /* ---- box in grid for mandelbrot set ---- */
 	
-	box_1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	box_1 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 	gtk_grid_attach(GTK_GRID(grid), box_1, 0, 0, 1, 1);
+	
+	image = gtk_image_new_from_file(".out.ppm");
+	gtk_box_pack_start(GTK_BOX(box_1), image, FALSE, FALSE, 0);
 	
 /* ---- box for image separator ---- */
 	
 	box_2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-	gtk_grid_attach(GTK_GRID(grid), box_2, 1, 0, 1, 15);
+	gtk_grid_attach(GTK_GRID(grid), box_2, 1, 0, 1, 16);
 	
 /* ---- add a seperator ---- */
 	
@@ -103,8 +105,8 @@ static void activate (GtkApplication *app, gpointer data)
 	
 /* ---- box in grid for buttons ---- */
 	
-	box_3 = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-	gtk_grid_attach(GTK_GRID(grid), box_3, 6, 0, 1, 15);
+	box_3 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+	gtk_grid_attach(GTK_GRID(grid), box_3, 2, 0, 1, 15);
 	
 	name_iterations = gtk_label_new("Iterations");
 	name_offset_x = gtk_label_new("Offset X");
@@ -112,11 +114,11 @@ static void activate (GtkApplication *app, gpointer data)
 	name_zoom = gtk_label_new("Zoomfactor");
 	name_color = gtk_label_new("Colortemplate");
 	
-	gtk_widget_set_halign(name_iterations, GTK_ALIGN_END);
-	gtk_widget_set_halign(name_offset_x, GTK_ALIGN_END);
-	gtk_widget_set_halign(name_offset_y, GTK_ALIGN_END);
-	gtk_widget_set_halign(name_zoom, GTK_ALIGN_END);
-	gtk_widget_set_halign(name_color, GTK_ALIGN_END);
+	gtk_widget_set_halign(name_iterations, GTK_ALIGN_CENTER);
+	gtk_widget_set_halign(name_offset_x, GTK_ALIGN_CENTER);
+	gtk_widget_set_halign(name_offset_y, GTK_ALIGN_CENTER);
+	gtk_widget_set_halign(name_zoom, GTK_ALIGN_CENTER);
+	gtk_widget_set_halign(name_color, GTK_ALIGN_CENTER);
 	
 	gtk_widget_set_name(name_iterations, "style_output");
 	gtk_widget_set_name(name_offset_x, "style_output");
@@ -130,11 +132,11 @@ static void activate (GtkApplication *app, gpointer data)
 	gtk_widget_set_size_request(name_zoom, 50, 40);
 	gtk_widget_set_size_request(name_color, 50, 40);
 	
-	gtk_grid_attach(GTK_GRID(grid), name_iterations, 6, 1, 1, 1);
-	gtk_grid_attach(GTK_GRID(grid), name_offset_x, 6, 4, 1, 1);
-	gtk_grid_attach(GTK_GRID(grid), name_offset_y, 6, 7, 1, 1);
-	gtk_grid_attach(GTK_GRID(grid), name_zoom, 6, 10, 1, 1);
-	gtk_grid_attach(GTK_GRID(grid), name_color, 6, 12, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), name_iterations, 2, 1, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), name_offset_x, 2, 4, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), name_offset_y, 2, 7, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), name_zoom, 2, 10, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), name_color, 2, 12, 1, 1);
 	
 /* ---- text entry with placeholder text ---- */
 	
@@ -143,27 +145,15 @@ static void activate (GtkApplication *app, gpointer data)
 	local_data->input_offset_y = gtk_entry_new();
 	local_data->input_zoom = gtk_entry_new();
 	
-	gtk_grid_attach(GTK_GRID(grid), local_data->input_iterations, 6, 2, 1, 1);
-	gtk_grid_attach(GTK_GRID(grid), local_data->input_offset_x, 6, 5, 1, 1);
-	gtk_grid_attach(GTK_GRID(grid), local_data->input_offset_y, 6, 8, 1, 1);
-	gtk_grid_attach(GTK_GRID(grid), local_data->input_zoom, 6, 11, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), local_data->input_iterations, 2, 2, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), local_data->input_offset_x, 2, 5, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), local_data->input_offset_y, 2, 8, 1, 1);
+	gtk_grid_attach(GTK_GRID(grid), local_data->input_zoom, 2, 11, 1, 1);
 	
 	gtk_entry_set_placeholder_text(GTK_ENTRY(local_data->input_iterations), "e.g. 1000");
 	gtk_entry_set_placeholder_text(GTK_ENTRY(local_data->input_offset_x), "e.g. -0.5");
 	gtk_entry_set_placeholder_text(GTK_ENTRY(local_data->input_offset_y), "e.g. -0.005");
 	gtk_entry_set_placeholder_text(GTK_ENTRY(local_data->input_zoom), "e.g. -0.99995");
-	
-/* ---- separators for each input value ---- */
-	
-/*	sep_iterations = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);*/
-/*	sep_offset_x = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);*/
-/*	sep_offset_y = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);*/
-/*	sep_zoom = gtk_separator_new(GTK_ORIENTATION_HORIZONTAL);*/
-/*	*/
-/*	gtk_grid_attach(GTK_GRID(grid), sep_iterations, 6, 0, 1, 1);*/
-/*	gtk_grid_attach(GTK_GRID(grid), sep_offset_x, 6, 3, 1, 1);*/
-/*	gtk_grid_attach(GTK_GRID(grid), sep_offset_y, 6, 6, 1, 1);*/
-/*	gtk_grid_attach(GTK_GRID(grid), sep_zoom, 6, 9, 1, 1);*/
 	
 /* ---- connect a signal when ENTER is hit within the entry box ---- */
 	
@@ -208,6 +198,20 @@ static void activate (GtkApplication *app, gpointer data)
 /* ---- connect a signal when the GENERATE button is clicked ---- */
 	
 	//g_signal_connect(generate_button, "clicked", G_CALLBACK(calculation), (gpointer)local_data);
+	
+/* ---- put save button to the right side of the header bar ---- */
+	
+	save_button = gtk_button_new_with_mnemonic("_Save Picture");
+	context = gtk_widget_get_style_context(save_button);
+	
+	gtk_style_context_add_class(context, "text-button");
+	gtk_style_context_add_class(context, "sugested-action");
+	
+	gtk_header_bar_pack_end(GTK_HEADER_BAR(headerbar), save_button);
+	
+/* ---- connect a signal when the SAVE button is clicked ---- */
+	
+	//g_signal_connect(save_button, "clicked", G_CALLBACK(saveimage), (gpointer)local_data);
 	
 /* ---- add styles ---- */
 	
